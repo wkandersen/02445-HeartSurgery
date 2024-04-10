3e0199f2b902ab4a4c825308a70b21ebdc857842
Kirk <s224225@student.dtu.dk>
2024-04-10 17:38:42 +0200

    Opdater special tegn

--- Data_processing.py
+++ Data_processing.py
@@ -1,16 +1,16 @@
 def Dates_heart_op(IDno):
     operationsdatoer = pd.unique(OE1O_processed[OE1O_processed['IDno'] == IDno]['Operationsdato'].dt.date)
-    Hændelsestidspunkt = Anæstesihændelse_processed[Anæstesihændelse_processed[Anæstesihændelse_processed['IDno'] == ID]['Timestamp'].dt.date.isin(operationsdatoer)]
-    return Hændelsestidspunkt
+    H?ndelsestidspunkt = An?stesih?ndelse_processed[An?stesih?ndelse_processed[An?stesih?ndelse_processed['IDno'] == ID]['Timestamp'].dt.date.isin(operationsdatoer)]
+    return H?ndelsestidspunkt
 
 def Inddeling(IDno):
     Dates_heart_op(IDno)
-    Induktion = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Induktion']['Timestamp']
-    Bypass_start = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass start']['Timestamp']
-    Aorta_tang_på = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang på']['Timestamp']
-    Aorta_tang_af = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang af']['Timestamp']
-    Bypass_slut = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass slut']['Timestamp']
-    Stop_indsaml = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Stop Data Indsamling']['Timestamp']
+    Induktion = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'Induktion']['Timestamp']
+    Bypass_start = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'CV Bypass start']['Timestamp']
+    Aorta_tang_p? = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'Aorta tang p?']['Timestamp']
+    Aorta_tang_af = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'Aorta tang af']['Timestamp']
+    Bypass_slut = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'CV Bypass slut']['Timestamp']
+    Stop_indsaml = H?ndelsestidspunkt[H?ndelsestidspunkt['H?ndelse'] == 'Stop Data Indsamling']['Timestamp']
 
 def Induktion_timestamps(IDno):
     AHM_ID = AHM_processed[AHM_processed['IDno'] == IDno]
