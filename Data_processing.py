da49b94b832785e174c4264db0effa5d1a955ad6
kris907j <s224197@dtu.dk>
2024-04-10 18:01:16 +0200

    Created function for getting timestamp for induction

--- Data_processing.py
+++ Data_processing.py
@@ -1,16 +1,20 @@
-def Dates_heart_op(IDno):
-    operationsdatoer = pd.unique(OE1O_processed[OE1O_processed['IDno'] == IDno]['Operationsdato'].dt.date)
-    Hændelsestidspunkt = Anæstesihændelse_processed[Anæstesihændelse_processed[Anæstesihændelse_processed['IDno'] == ID]['Timestamp'].dt.date.isin(operationsdatoer)]
-    return Hændelsestidspunkt
+class Data_processing():    
+    def Dates_heart_op(IDno):
+        operationsdatoer = pd.unique(OE1O_processed[OE1O_processed['IDno'] == IDno]['Operationsdato'].dt.date)
+        Hændelsestidspunkt = Anæstesihændelse_processed[Anæstesihændelse_processed[Anæstesihændelse_processed['IDno'] == ID]['Timestamp'].dt.date.isin(operationsdatoer)]
+        return Hændelsestidspunkt
 
-def Inddeling(IDno):
-    Dates_heart_op(IDno)
-    Induktion = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Induktion']['Timestamp']
-    Bypass_start = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass start']['Timestamp']
-    Aorta_tang_på = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang på']['Timestamp']
-    Aorta_tang_af = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang af']['Timestamp']
-    Bypass_slut = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass slut']['Timestamp']
-    Stop_indsaml = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Stop Data Indsamling']['Timestamp']
+    def Inddeling(IDno):
+        Dates_heart_op(IDno)
+        Induktion = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Induktion']['Timestamp']
+        Bypass_start = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass start']['Timestamp']
+        Aorta_tang_på = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang på']['Timestamp']
+        Aorta_tang_af = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Aorta tang af']['Timestamp']
+        Bypass_slut = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'CV Bypass slut']['Timestamp']
+        Stop_indsaml = Hændelsestidspunkt[Hændelsestidspunkt['Hændelse'] == 'Stop Data Indsamling']['Timestamp']
 
-def Induktion_timestamps(IDno):
-    AHM_ID = AHM_processed[AHM_processed['IDno'] == IDno]
+    def Induktion_timestamps(IDno):
+        AHM_ID = AHM_processed[AHM_processed['IDno'] == IDno]
+        AHM_ID = AHM_ID.set_index(['Timestamp'])
+        Induk = AHM_ID.loc[Induction.iloc[0]:(Bypass_start).iloc[0]]
+        Induk = Induk.drop(Induk.index[-1])
