d2c07d9bf67d3e2320da6bc5b93d71b4e8d2a05a
Nicolai Munksgaard Schmidt <nicolai.munksgaard.schmidt@regionh.dk>
2024-06-12 09:55:09 +0200

    Opdatering af dead30, dead90 som skal bruges til vores y-vektor

--- data_processing_som_py.py
+++ data_processing_som_py.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-titles = ["ADT", "AHM", "AoR", "OE1O","Anæstesidata","Anæstesihændelse", "Blødning", "ca_vitale_værdier",  "Dialyse", "EKG_data", "Ekkokardiografi", "Højde", "INC", "intellispace", "ITA", "KAG_data", "Lab_svar", "medicin", "Pleuradræn", "Populationen", "Problemliste","Respirator_data", "Samlet_udskillelse", "Spiromitri", "Total_indgift", "Urin", "Vægt", "viewpoint"]
-# "ADT", "AHM", "AoR", "OE1O","Anæstesidata","Anæstesihændelse", "Blødning", "ca_vitale_værdier",  "Dialyse", "EKG_data", "Ekkokardiografi", "Højde", "INC", "intellispace", "ITA", "KAG_data", "Lab_svar", "medicin", "Pleuradræn", "Populationen", "Problemliste","Respirator_data", "Samlet_udskillelse", "Spiromitri", "Total_indgift", "Urin", "Vægt", "viewpoint"
+titles = ['ADT' ,  'AHM' ,  'AoR' ,  'OE1O' , 'Anæstesidata' , 'Anæstesihændelse' ,  'Blødning' ,  'ca_vitale_værdier' ,   'Dialyse' ,  'EKG_data' ,  'Ekkokardiografi' ,  'Højde' ,  'intellispace' ,  'ITA' ,  'KAG_data' ,  'Lab_svar' ,  'medicin' ,  'Pleuradræn' ,  'Populationen' ,  'Problemliste' , 'Respirator_data' ,  'Samlet_udskillelse' ,  'Spiromitri' ,  'Total_indgift' ,  'Urin' ,  'Vægt' ,  'viewpoint' ]
+
 for title in titles:
     parquet_file = f'Nye_parquet_files\{title}.parquet.gzip'
     df = pd.read_parquet(parquet_file)
@@ -16,6 +16,55 @@ for title in titles:
         globals()[title]['Indlæggelsestidspunkt']=pd.to_datetime(globals()[title]['Indlæggelsestidspunkt'])
         globals()[title]['Udskrivelsestidspunkt']=pd.to_datetime(globals()[title]['Udskrivelsestidspunkt'])
         globals()[title]['Operationsdato']=pd.to_datetime(globals()[title]['Operationsdato'])
+        globals()[title]['Dødsdato']=pd.to_datetime(globals()[title]['Dødsdato'])
+
+        dead30 = []
+        dead90 = []
+        dead365 = []
+        for i in range(len(globals()[title]["IDno"])):
+            if globals()[title]["Død inden for 1 år af operation"][i] == "Nej":
+                dead30.append(np.nan)
+                dead90.append(np.nan)
+                dead365.append(np.nan)
+                continue
+            else:
+                difference = ((pd.to_datetime(globals()[title]["Dødsdato"][i]).date()-globals()[title]["Operationsdato"][i].date()).days)
+                if difference <31:
+                    dead30.append(difference)
+                    dead90.append(np.nan)
+                    dead365.append(np.nan)
+                elif 31<= difference < 90:
+                    dead30.append(np.nan)
+                    dead90.append(difference)
+                    dead365.append(np.nan)
+                else:
+                    dead30.append(np.nan)
+                    dead90.append(np.nan)
+                    dead365.append(difference)
+                
+        globals()[title]["dead30"]=dead30
+        globals()[title]["dead90"]=dead90
+        globals()[title]["dead365"]=dead365
+
+        dead_days = []
+
+        for i in range(len(globals()[title]["IDno"])):
+            if globals()[title]["Død inden for 1 år af operation"][i] == "Nej":
+                dead_days.append(0)
+                continue
+            else:
+                difference = ((pd.to_datetime(globals()[title]["Dødsdato"][i]).date()-globals()[title]["Operationsdato"][i].date()).days)
+                if difference <31:
+                    dead_days.append(1)
+                elif 31<= difference < 90:
+                    dead_days.append(2)
+                else:
+                    dead_days.append(3)
+                
+        globals()[title]["dead_days"]=dead_days
+
+
+
 
     elif title == 'AHM':
         globals()[title]['Enhed'] = globals()[title]['Enhed'].fillna('Ikke angivet')
@@ -87,6 +136,14 @@ for title in titles:
 
         globals()[title] = pd.DataFrame(data, columns=['Genstande per uge', 'Rygning', 'IDno'])
 
+    # elif title == 'Lab_svar':
+    #     globals()[title] = globals()[title].rename(columns={ 'V2' : 'What is being measured' ,  'V3' :  'Timestamp' ,  'V4' :  'Results' })
+    #     globals()[title] = globals()[title].dropna(subset=['Results'])
+    #     globals()[title]['Results'] = pd.to_numeric(globals()[title]['Results'], errors='coerce')
+    #     globals()[title] = globals()[title].groupby(['Timestamp','IDno','What is being measured']).Results.mean().reset_index()
+    #     globals()[title] = globals()[title].pivot(index = ['IDno','Timestamp'], columns=['What is being measured'],values='Results')
+    #     globals()[title].reset_index(inplace=True)
+    #     globals()[title]['Timestamp'] = pd.to_datetime(globals()[title]['Timestamp'])
     
     elif title == 'Lab_svar':
         globals()[title] = globals()[title].rename(columns={ 'Best./ord. navn' : 'What is being measured' ,  'Resultatdatotid' :  'Timestamp' ,  'Resultatværdi' :  'Results' })
